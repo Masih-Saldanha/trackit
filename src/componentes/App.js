11476313a8ffbbe26ecc2c7f45286d07a35bf484
Masih-Saldanha <masih.saldanha@gmail.com>
2022-03-28 23:14:33 -0300

    App, Home e Cadastro criados, falta conteudo

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Cadastro from "./Cadastro";
+
 export default function App() {
     return (
-        <h1>teste</h1>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/cadastro/" element={<Cadastro />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
