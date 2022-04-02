bd6bea1088155570de09d90ba1b5ba22b7750164
Masih-Saldanha <masih.saldanha@gmail.com>
2022-04-02 11:32:14 -0300

    Marcação de Hábitos de Hoje concluído, restam porcentagem

--- src/componentes/HojeLista.js
+++ src/componentes/HojeLista.js
@@ -1,30 +1,75 @@
-import { useState } from "react";
+import { useContext } from "react";
+import axios from "axios";
 import styled from "styled-components";
 import check_marca from "../assets/checkbox-marca.svg";
+import TokenContext from "../contextos/TokenContext";
 
 export default function HojeLista({ id, name, done, currentSequence, highestSequence }) {
-    const [cor, setCor] = useState("#EBEBEB")
-    const [selecionado, setSelecionado] = useState(false);
+    const { token, receberHabitosHoje } = useContext(TokenContext);
+
+    function marcarDesmarcarHabito() {
+        if (done === false) {
+            const url =
+                `https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${id}/check`;
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            };
+            const promise = axios.post(url, true, config);
+            promise.then((response) => {
+                receberHabitosHoje();
+            });
+            promise.catch((err) => {
+                const { response } = err;
+                const { data } = response
+                const { message } = data;
+                alert(message);
+            });
+        } else {
+            const url =
+                `https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${id}/uncheck`;
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            };
+            const promise = axios.post(url, false, config);
+            promise.then((response) => {
+                receberHabitosHoje();
+            });
+            promise.catch((err) => {
+                const { response } = err;
+                const { data } = response
+                const { message } = data;
+                alert(message);
+            });
+        }
+    }
 
     return (
-        <CaixaHabitoHoje cor={cor}>
-            <section>
-                <h2>{name}</h2>
-                <h3>Sequência atual: {currentSequence} dias</h3>
-                <h3>Seu recorde: {highestSequence} dias</h3>
-            </section>
-            <figure onClick={() => {
-                if (selecionado) {
-                    setCor("#EBEBEB");
-                    setSelecionado(false);
-                } else {
-                    setCor("#8FC549");
-                    setSelecionado(true);
-                }
-            }}>
-                <img src={check_marca} alt="Caixa de Marcar" />
-            </figure>
-        </CaixaHabitoHoje>
+        done === false ?
+            <CaixaHabitoHoje>
+                <section>
+                    <h2>{name}</h2>
+                    <h3>Sequência atual: {currentSequence} dias</h3>
+                    <h3>Seu recorde: {highestSequence} dias</h3>
+                </section>
+                <Desmarcado onClick={marcarDesmarcarHabito}>
+                    <img src={check_marca} alt="Caixa de Marcar" />
+                </Desmarcado>
+            </CaixaHabitoHoje>
+            :
+            <CaixaHabitoHoje>
+                <section>
+                    <h2>{name}</h2>
+                    <h3>Sequência atual<MarcadoSequencia>: {currentSequence} dias</MarcadoSequencia></h3>
+                    {currentSequence === highestSequence ? <h3>Seu recorde<MarcadoSequencia>: {highestSequence} dias</MarcadoSequencia></h3> : <h3>Seu recorde: {highestSequence} dias</h3>}
+                </section>
+                <Marcado onClick={marcarDesmarcarHabito}>
+                    <img src={check_marca} alt="Caixa de Marcar" />
+                </Marcado>
+            </CaixaHabitoHoje>
     )
 }
 
@@ -42,21 +87,41 @@ section {
         margin-bottom: 10px;
     }
     h3 {
+        display: inline-flex;
         font-size: 13px;
         margin-bottom: 3px;
     }
 }
-figure {
-    width: 69px;
-    height: 69px;
-    background: ${(props) => props.cor};
-    border: 1px solid #E7E7E7;
-    border-radius: 5px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    img {
-        height: 28px;
-    }
+`
+
+const Desmarcado = styled.figure`
+width: 69px;
+height: 69px;
+background: #EBEBEB;
+border: 1px solid #E7E7E7;
+border-radius: 5px;
+display: flex;
+justify-content: center;
+align-items: center;
+img {
+    height: 28px;
 }
+`
+
+const Marcado = styled.figure`
+width: 69px;
+height: 69px;
+background: #8FC549;
+border: 1px solid #E7E7E7;
+border-radius: 5px;
+display: flex;
+justify-content: center;
+align-items: center;
+img {
+    height: 28px;
+}
+`
+
+const MarcadoSequencia = styled.h3`
+color: #8FC549;
 `
